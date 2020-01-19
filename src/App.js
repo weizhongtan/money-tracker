4801293adf43bc384ea9997ff9a1ba7de7b107d1
weizhongtan <me@weizhongtan.com>
2020-01-19 23:06:27 +0000

    apollo-react poc

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useLayoutEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,6 +12,7 @@ import { Segment, Menu } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 import Navigation from './Navigation';
 // import Graphs from './Graphs';
@@ -29,52 +31,61 @@ const Main = styled(Segment)`
   width: 100%;
 `;
 
-function App() {
-  useEffect(() => {
-    const get = async () => {
-      const res = await client.query({
-        query: gql`
-          {
-            allAccounts {
-              nodes {
-                rowId
-              }
-            }
-          }
-        `,
-      });
+const ALL_ACCOUNTS = gql`
+  {
+    allAccounts {
+      nodes {
+        rowId
+      }
+    }
+  }
+`;
 
-      console.log(res);
-    };
-    get();
-  });
+const Inner = () => {
+  const { loading, error, data } = useQuery(ALL_ACCOUNTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
   return (
-    <Wrapper>
-      <Router>
-        <Navigation>
-          {({ location }) => (
-            <Menu tabular>
-              <Menu.Item active={location.pathname === '/transactions'}>
-                <NavLink exact to="/transactions">
-                  Transactions
-                </NavLink>
-              </Menu.Item>
-              <Menu.Item active={location.pathname === '/graphs'}>
-                <NavLink to="/graphs">Graphs</NavLink>
-              </Menu.Item>
-            </Menu>
-          )}
-        </Navigation>
-        <Main attached="bottom">
-          <Switch>
-            {/* <Route path="/transactions" exact component={Transactions} /> */}
-            {/* <Route path="/graphs" exact component={Graphs} /> */}
-            <Redirect to="/transactions" />
-          </Switch>
-        </Main>
-      </Router>
-    </Wrapper>
+    <ul>
+      {data.allAccounts.nodes.map(({ rowId }) => (
+        <li>{rowId}</li>
+      ))}
+    </ul>
+  );
+};
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Wrapper>
+        <Router>
+          <Navigation>
+            {({ location }) => (
+              <Menu tabular>
+                <Menu.Item active={location.pathname === '/transactions'}>
+                  <NavLink exact to="/transactions">
+                    Transactions
+                  </NavLink>
+                </Menu.Item>
+                <Menu.Item active={location.pathname === '/graphs'}>
+                  <NavLink to="/graphs">Graphs</NavLink>
+                </Menu.Item>
+              </Menu>
+            )}
+          </Navigation>
+          <Main attached="bottom">
+            <Switch>
+              <Inner />
+              {/* <Route path="/transactions" exact component={Transactions} /> */}
+              {/* <Route path="/graphs" exact component={Graphs} /> */}
+              <Redirect to="/transactions" />
+            </Switch>
+          </Main>
+        </Router>
+      </Wrapper>
+    </ApolloProvider>
   );
 }
 
