0d1d49620ec8abeb1d2e14a859ae88422cbf3f8a
weizhongtan <me@weizhongtan.com>
2020-07-18 17:02:22 +0100

    remove legacy homebank migration files

--- src/api/migration/json-to-postgres.js
+++
@@ -1,7 +0,0 @@
-const { connectAndSync, addExistingData } = require('../lib');
-const data = require('../../../private/bank');
-
-(async () => {
-  await connectAndSync(true);
-  await addExistingData(data);
-})();
